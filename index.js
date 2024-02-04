437b868897383c2c98f6db568cbd95b18a6e8f16
Shan <shansiddiqui94@gmail.com>
2024-02-04 01:14:05 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400){
+    return "This one is on me!";
+}
+else if(ride <= 2000 && ride >= 400){
+    return 'That will be twenty bucks.'
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+else if (ride > 2000 && ride < 2500){
+return "I will gladly take your thirty bucks.";
+}
+else if(ride > 2500){
+return "No can do.";
+}
 }
 
-function switchOnCharmFromTip(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+  if(city === "NYC"){
+    return "Ok, sounds good."
+  }
+  else{
+   return "No go."
+  }
+}
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  if(tip === "generous"){
+    return "Thank you so much."
+  }
+  else if(tip === "not as generous" ){
+    return "Thank you."
+  }
+  else{
+   return "Bye."
+  }
 }
